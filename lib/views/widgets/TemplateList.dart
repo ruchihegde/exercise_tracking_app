b42d008333f2352f3d33f6bd63cb36169a0f0d55
Kyle Bello <112905761+HeyKile@users.noreply.github.com>
2024-11-05 17:19:32 -0500

    added conditional logic for if in workout view or template view

--- lib/views/widgets/TemplateList.dart
+++ lib/views/widgets/TemplateList.dart
@@ -1,7 +1,7 @@
 import 'package:exercise_tracking_app/views/widgets/TemplateListItem.dart';
 import 'package:flutter/material.dart';
-import '../../services/TemplateService.dart';
 import '../../viewmodels/TemplateViewModel.dart';
+import '../../models/TemplateModel.dart';
 import 'package:provider/provider.dart';
 
 enum TemplateTab { myTemplates, premade }
@@ -40,68 +40,111 @@ class _TemplateListState extends State<TemplateList> {
     templateViewModel.setSearchQuery(searchController.text);
   }
 
+  VoidCallback chooseTemplate(Template? template) {
+    return () {
+      if (template == null) {
+        print("chosen blank!");
+      }
+      else {
+        print('Chosen ${template.name}');
+      }
+    };
+  }
+
   @override
   Widget build(BuildContext context) {
     // TODO: add control for when workout or not
     return Consumer<TemplateViewModel>(
       builder: (context, templateViewModel, child) {
-        return Center(
-          child: Column(
-            children: [
-              Padding(
-                padding: const EdgeInsets.all(10.0),
-                child: SizedBox(
+        return Scaffold(
+          body: Center(
+            child: Column(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(10.0),
+                  child: SizedBox(
+                    width: MediaQuery.of(context).size.width * 0.8,
+                    child: TextField(
+                    controller: searchController,
+                    decoration: InputDecoration(
+                      labelText: 'Search templates',
+                      prefixIcon: const Icon(Icons.search),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(8.0),
+                      ),
+                    ),
+                  ),
+                  )
+                ),
+                SizedBox(
                   width: MediaQuery.of(context).size.width * 0.8,
-                  child: TextField(
-                  controller: searchController,
-                  decoration: InputDecoration(
-                    labelText: 'Search templates',
-                    prefixIcon: const Icon(Icons.search),
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(8.0),
+                  child: SegmentedButton<TemplateTab>(
+                    segments: const <ButtonSegment<TemplateTab>>[
+                      ButtonSegment<TemplateTab>(
+                        value: TemplateTab.myTemplates,
+                        label: Text('My Templates'),
+                      ),
+                      ButtonSegment<TemplateTab>(
+                        value: TemplateTab.premade,
+                        label: Text('Premade Templates'),
+                      )
+                    ],
+                    selected: {templateViewModel.currentTab},
+                    onSelectionChanged: (Set<TemplateTab> newSelection) {
+                      templateViewModel.setTab(newSelection.first);
+                    },
+                    showSelectedIcon: false,
+                  )
+                ),
+                const SizedBox(height: 10),
+                Visibility(
+                  visible: widget.isWorkout,
+                  child: Center(
+                    child: TextButton(
+                      onPressed: chooseTemplate(null),
+                      child: const Row(
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          Icon(Icons.add),
+                          Text("Use Blank Template"),
+                        ],
+                      ),
                     ),
                   ),
                 ),
-                )
+                Expanded(
+                  child: templateViewModel.filteredTemplates.isNotEmpty
+                    ? ListView.builder(
+                        itemCount: templateViewModel.filteredTemplates.length,
+                        itemBuilder: (context, idx) {
+                          final template = templateViewModel.filteredTemplates[idx];
+                          return TemplateListItem(
+                            template: template,
+                            onTap: chooseTemplate(template),
+                            isWorkout: false,
+                          );
+                        }
+                      )
+                    : const Text('No Templates Found')
+                ),
+              ],
+            )
+          ),
+          floatingActionButton: Visibility(
+            visible: !widget.isWorkout,
+            child: FloatingActionButton(
+              onPressed: () {
+                print("clicked!");
+              },
+              backgroundColor: Colors.blue,
+              foregroundColor: Colors.white,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(15.0),
               ),
-              SizedBox(
-                width: MediaQuery.of(context).size.width * 0.8,
-                child: SegmentedButton<TemplateTab>(
-                  segments: const <ButtonSegment<TemplateTab>>[
-                    ButtonSegment<TemplateTab>(
-                      value: TemplateTab.myTemplates,
-                      label: Text('My Templates'),
-                    ),
-                    ButtonSegment<TemplateTab>(
-                      value: TemplateTab.premade,
-                      label: Text('Premade Templates'),
-                    )
-                  ],
-                  selected: {templateViewModel.currentTab},
-                  onSelectionChanged: (Set<TemplateTab> newSelection) {
-                    templateViewModel.setTab(newSelection.first);
-                  },
-                  showSelectedIcon: false,
-                )
-              ),
-              const SizedBox(height: 10),
-              Expanded(
-                child: templateViewModel.filteredTemplates.isNotEmpty
-                  ? ListView.builder(
-                      itemCount: templateViewModel.filteredTemplates.length,
-                      itemBuilder: (context, idx) {
-                        final template = templateViewModel.filteredTemplates[idx];
-                        return TemplateListItem(
-                          template: template,
-                          onTap: () => print('tapped ${template.name}'),
-                          isWorkout: false,
-                        );
-                      }
-                    )
-                  : const Text('No Templates Found')
-              )
-            ],
-          )
+              child: const Icon(Icons.add),
+            ),
+          ),
+          floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
         );
       },
     );
